ebcc918c3d425735426c1c60315ccc74cf35e5d3
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-12 11:37:50 -0300

    Display the blog posts on the page, and add minimum styling.

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((response) => response.json())
-  .then((posts) => console.log(posts));
+  .then((posts) => {
+    const postsArr = posts.slice(0, 5);
+    let html = "";
+
+    for (const post of postsArr) {
+      html += `
+      <article class="post" >
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        <hr />
+      </article>
+      `;
+    }
+
+    document.querySelector("#posts").innerHTML = html;
+  });
